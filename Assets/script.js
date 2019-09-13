f1d5ab768d78dd061ab776b411fb4cff8dac66e8
LoicVanRompaey <loic.vanrompaey@gmail.com>
2019-09-13 11:40:31 +0200

    added lizard and spock, trying to get counter to work

--- Assets/script.js
+++ Assets/script.js
@@ -1,6 +1,11 @@
 var choice;
-var AIchoice = ["rock", "paper", "scissors"];
+var AIchoice = ["rock", "paper", "scissors", "lizard", "spock"];
 var AIsign;
+var win = 0;
+var loss = 0;
+var wincounter = document.getElementById("win");
+var losscounter = document.getElementById("loss");
+
 
 (function () {
 
@@ -10,73 +15,165 @@ var AIsign;
 
     document.getElementById("rock").addEventListener("click", function () {
         choice = "rock";
-        AIsign = AIchoice[randomGenerateInt(0, 2)];
+        AIsign = AIchoice[randomGenerateInt(0, 4)];
         console.log(choice);
         console.log(AIsign);
 
         if (AIsign === "paper") {
-            alert("you lose!")
+            alert("you lose!");
+            loss++;
+            console.log(loss)
         }
 
         if (AIsign === "scissors") {
-            alert(" you win!")
+            alert(" you win!");
+            win++;
+            console.log(win);
         }
 
         if (choice === AIsign) {
             alert("It's a tie!")
+        }
+
+        if (AIsign === "lizard") {
+            alert("you win");
+            win++;
+            console.log(win)
+        }
+
+        if (AIsign === "spock") {
+            alert("you lose");
+            loss++;
+            console.log(loss);
         }
 
     });
 
     document.getElementById("paper").addEventListener("click", function () {
         choice = "paper";
-        AIsign = AIchoice[randomGenerateInt(0, 2)];
+        AIsign = AIchoice[randomGenerateInt(0, 4)];
         console.log(choice);
         console.log(AIsign);
 
         if (AIsign === "rock") {
-            alert("you win!")
+            alert("you win!");
+            win++
         }
 
         if (AIsign === "scissors") {
-            alert("you lose!")
+            alert("you lose!");
+            loss++
         }
 
-        if (choice === AIsign) {
+        if (AIsign === "paper") {
             alert("It's a tie!")
+        }
+
+        if (AIsign === "lizard") {
+            alert("you lose")
+        }
+
+        if (AIsign === "spock") {
+            alert("you win");
+            win++
         }
     });
 
     document.getElementById("scissors").addEventListener("click", function () {
         choice = "scissors";
-        AIsign = AIchoice[randomGenerateInt(0, 2)];
+        AIsign = AIchoice[randomGenerateInt(0, 4)];
         console.log(choice);
         console.log(AIsign);
 
 
         if (AIsign === "rock") {
-            alert("you lose!")
+            alert("you lose!");
+            loss++
         }
 
         if (AIsign === "paper") {
-            alert("you win!")
+            alert("you win!");
+            win++
         }
         if (choice === AIsign) {
             alert("It's a tie!")
         }
-    });
 
-    document.getElementById("mystery").addEventListener("click", function () {
-        var gamble = randomGenerateInt(0,20);
-        console.log(gamble);
-
-        if(gamble >10 && gamble %2 === 0){
-            alert("BOOM, you nuked the enemy")
-        }else{
-            alert("You got WRECKED son! I think a midget even stole your wallet while you were getting your ass kicked!")
+        if (AIsign === "lizard") {
+            alert("you win");
+            win++
         }
 
+        if (AIsign === "spock") {
+            alert("you lose");
+            loss++
+        }
+    });
 
-    })
+    document.getElementById("lizard").addEventListener("click", function () {
+        choice = "lizard";
+        AIsign = AIchoice[randomGenerateInt(0, 4)];
+        console.log(choice);
+        console.log(AIsign);
+
+        if (AIsign === "rock") {
+            alert("you lose!");
+            loss++
+        }
+
+        if (AIsign === "paper") {
+            alert("you win!");
+            win++
+        }
+        if (choice === "scissors") {
+            alert("you lose");
+            loss++
+        }
+
+        if (AIsign === choice) {
+            alert("it's a tie")
+        }
+
+        if (AIsign === "spock") {
+            alert("you win");
+            win++
+        }
+
+    });
+
+    document.getElementById("spock").addEventListener("click", function () {
+        choice = "spock";
+        AIsign = AIchoice[randomGenerateInt(0, 4)];
+        console.log(choice);
+        console.log(AIsign);
+
+        if (AIsign === "rock") {
+            alert("you win");
+            win++
+        }
+
+        if (AIsign === "paper") {
+            alert("you lose!");
+            loss++
+        }
+        if (choice === "scissors") {
+            alert("you win!");
+            win++
+        }
+
+        if (AIsign === "lizard") {
+            alert("you lose");
+            loss++
+        }
+
+        if (AIsign === choice) {
+            alert("it's a tie")
+        }
+
+    });
+
+    wincounter = win;
+    losscounter= loss;
+
 
 })();
